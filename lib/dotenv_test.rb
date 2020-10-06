2a08ee1d44b6d6ba65d3252fa42aa55542f590d3
Iris Lux <iris.lux0@gmail.com>
2020-10-05 20:45:59 -0700

    updated doteenv_test

--- lib/dotenv_test.rb
+++ lib/dotenv_test.rb
@@ -1,8 +1,10 @@
 # dotenv_practice.rb
 require 'dotenv'
-
+require 'httparty'
 # Tell dotenv to look for the .env file
 Dotenv.load
 
-p ENV["SLACK_API_TOKEN"]
+puts
+
+channels = HTTParty.get('https://slack.com/api/conversations.list', query: {token: ENV["SLACK_API_TOKEN"]})["channels"].each{|channel| puts channel["name"]}
 
