f5851a94b076b13eaaa48960e784e7859093e94a
Iris Lux <iris.lux0@gmail.com>
2020-10-05 21:03:59 -0700

    add VCR filter to test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,6 +9,8 @@ require 'minitest/reporters'
 require 'minitest/skip_dsl'
 require 'vcr'
 
+Dotenv.load
+
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 VCR.configure do |config|
@@ -25,5 +27,7 @@ VCR.configure do |config|
   }
 
   # Don't leave our token lying around in a cassette file.
-
+  config.filter_sensitive_data("<LOCATIONIQ_TOKEN>") do
+    ENV["LOCATIONIQ_TOKEN"]
+  end
 end
