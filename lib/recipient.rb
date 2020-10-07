695f40758e6779c3f7fae46395076a1a2ceed462
Iris Lux <iris.lux0@gmail.com>
2020-10-06 21:31:15 -0700

    took out arguemnts from constructor

--- lib/recipient.rb
+++ lib/recipient.rb
@@ -7,7 +7,7 @@ module SlackCLI
   class Recipient
 
     attr_reader :name, :slack_id
-    def initialize(slack_id, name)
+    def initialize(slack_id: , name: )
       @slack_id = slack_id
       @name = name
     end
@@ -22,7 +22,7 @@ module SlackCLI
       raise NotImplementedError, 'Implement me in a child class!'
     end
 
-    def list_all
+    def self.list_all
       raise NotImplementedError, 'Implement me in a child class!'
     end
 
