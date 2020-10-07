02cb3382e03424a1721dff5c03a8ccb6886ea38d
Iris Lux <iris.lux0@gmail.com>
2020-10-06 21:28:10 -0700

    added initilize tests

---
+++ test/channel_test.rb
@@ -0,0 +1,25 @@
+require_relative 'test_helper'
+
+describe 'Channel' do
+  describe "initialize" do
+    before do
+      @channel = SlackCLI::Channel.new()
+    end
+    it "constructor properly creates a Channel object" do
+      expect(@channel).must_be_instance_of SlackCLI::Channel
+    end
+
+    it "slack_id and name are both strings" do
+      expect(@channel.slack_id).must_be_instance_of String
+      expect(@channel.name).must_be_instance_of String
+    end
+
+    it "topic is a String" do
+      expect(@channel.topic).must_be_instance_of String
+    end
+
+    it "member count is an Integer" do
+      expect(@channel.member_count).must_be_instance_of String
+    end
+  end
+end
