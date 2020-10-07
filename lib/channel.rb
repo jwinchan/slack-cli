3b78f84a661f1465c89b5b8e7bdd3daedba30d4a
Jessica Chan <jwinchan@gmail.com>
2020-10-06 22:22:04 -0700

    created constructor method for channel

--- lib/channel.rb
+++ lib/channel.rb
@@ -1,5 +1,15 @@
+require_relative 'recipient'
+
 module SlackCLI
   class Channel < Recipient
+
+    attr_reader :slack_id, :name, :topic, :member_count
+
+    def initialize(slack_id:, name:, topic:, member_count:)
+      super(name, slack_id)
+      @topic = topic
+      @member_count = member_count
+    end
 
   end
 end
