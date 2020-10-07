70efd758876740cf9e10ed17e033c16b2996af21
Jessica Chan <jwinchan@gmail.com>
2020-10-07 16:38:03 -0700

    called to slack API to read conversations.list

--- lib/workspace.rb
+++ lib/workspace.rb
@@ -1,9 +1,19 @@
+require 'dotenv'
+require 'httparty'
+
+Dotenv.load
+
 class Workspace
   attr_reader :users, :channels
 
+  CHANNEL_PATH = "https://slack.com/api/conversations.list"
+
   def initialize
+    response = HTTParty.get(CHANNEL_PATH, query: {token: ENV["SLACK_API_TOKEN"]})
     @users = []
-    @channels = []
+    @channels = response["channels"]
   end
+
+
 
 end
