cfcd60c70fc073fb21d8ed1f4ca66eed21d718f6
Jessica Chan <jwinchan@gmail.com>
2020-10-06 22:22:46 -0700

    added constants to user method

--- lib/user.rb
+++ lib/user.rb
@@ -1,13 +1,28 @@
+require_relative 'recipient'
+
 module SlackCLI
   class User < Recipient
 
+    USER_URL = "https://slack.com/api/users.list"
+    PARAM = {token: ENV["SLACK_API_TOKEN"]}
+
     attr_reader :slack_id, :name, :real_name, :status_text, :status_emoji
 
-    def initialize(slack_id, name, real_name, status_text, status_emoji)
-      super(name, slack_id)
+    def initialize(slack_id:, name:, real_name:, status_text:, status_emoji:)
+      super(slack_id, name)
       @real_name = real_name
       @status_text = status_text
       @status_emoji = status_emoji
     end
+
+
+
+    private
+
+    def self.from_api(json_hash)
+
+    end
+
+
   end
 end
