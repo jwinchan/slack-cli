0d6d76c421a5c978b496530371582d22d8406e38
Jessica Chan <jwinchan@gmail.com>
2020-10-07 16:37:42 -0700

    created user menu

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,13 +1,32 @@
 #!/usr/bin/env ruby
+require 'dotenv'
+require 'httparty'
 require_relative 'workspace'
 
 def main
   puts "Welcome to the Ada Slack CLI!"
   workspace = Workspace.new
 
-  # TODO project
+  option = nil
+
+  until option == "quit" || option == "3"
+    if option == "list users" || option == "1"
+      puts workspace.users
+    elsif option == "list channels" || option == "2"
+      puts workspace.channels
+    end
+    menu
+    option = gets.chomp
+  end
 
   puts "Thank you for using the Ada Slack CLI"
+end
+
+def menu
+  puts "Please select one of the three options: "
+  puts "1. list users"
+  puts "2. list channels"
+  puts "3. quit"
 end
 
 main if __FILE__ == $PROGRAM_NAME
