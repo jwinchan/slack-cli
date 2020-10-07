295be86f5f3888e1c7e16cfacd4d840261d5bd8a
Iris Lux <iris.lux0@gmail.com>
2020-10-06 21:29:27 -0700

    addedNotIMplementedError tests

--- test/recipient_test.rb
+++ test/recipient_test.rb
@@ -2,29 +2,40 @@ require_relative 'test_helper'
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
+describe "Recipient" do
 
-describe "initialize" do
   before do
-    @recipient = SlackCLI::Recipient.new("1234ID", "Namey Nameson")
-  end
-  it "constructor properly creates a recipient object" do
-
-    expect(@recipient).must_be_instance_of SlackCLI::Recipient
+    @recipient = SlackCLI::Recipient.new()
   end
 
-  it "slack_id and name are both strings" do
-    expect(@recipient.slack_id).must_be_instance_of String
-    expect(@recipient.name).must_be_instance_of String
+  describe "initialize" do
+
+    it "constructor properly creates a recipient object" do
+
+      expect(@recipient).must_be_instance_of SlackCLI::Recipient
+    end
+
+    it "slack_id and name are both strings" do
+      expect(@recipient.slack_id).must_be_instance_of String
+      expect(@recipient.name).must_be_instance_of String
+    end
+  end
+
+  describe "self.get method" do
+    it "raises ArgumentError for incorrect parameters" do
+      expect{SlackCLI::Recipient.get("url", "not a hash")}.must_raise ArgumentError
+    end
+  end
+
+  describe 'details' do
+    it "raises error if not implemented" do
+      expect(@recipient.details).must_raise NotImplementedError
+    end
+  end
+
+  describe 'list_all' do
+    it "raises error if not implemented" do
+      expect(@recipient.list_all).must_raise NotImplementedError
+    end
   end
 end
-
-describe "self.get method" do
-  # before do
-  #   @recipient = SlackCLI::Recipient.new("1234ID", "Namey Nameson")
-  # end
-  it "raises ArgumentError for incorrect parameters" do
-    expect{SlackCLI::Recipient.get("url", "not a hash")}.must_raise ArgumentError
-  end
-end
-
-describe
