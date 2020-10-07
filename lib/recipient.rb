5d43a9a22b8f4f768fb15eb27d6827d916d0edff
Jessica Chan <jwinchan@gmail.com>
2020-10-06 20:34:58 -0700

    added get, details, and list_all methods

--- lib/recipient.rb
+++ lib/recipient.rb
@@ -1,3 +1,8 @@
+require 'dotenv'
+require 'httparty'
+
+Dotenv.load
+
 module SlackCLI
   class Recipient
 
@@ -6,5 +11,20 @@ module SlackCLI
       @slack_id = slack_id
       @name = name
     end
+
+    def self.get(url, params)
+      raise ArgumentError.new("invalid arguments") unless (url.is_a?(String) && params.is_a?(Hash))
+      api_info = HTTParty.get(url, query: params)
+      return api_info
+    end
+
+    def details
+      raise NotImplementedError, 'Implement me in a child class!'
+    end
+
+    def list_all
+      raise NotImplementedError, 'Implement me in a child class!'
+    end
+
   end
 end
