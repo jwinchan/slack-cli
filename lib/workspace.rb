9b88f1eace33a4fff94fda27237b3d2e4fabf518
Jessica Chan <jwinchan@gmail.com>
2020-10-07 20:37:20 -0700

    changed output statements and now calls from user and channel class

--- lib/workspace.rb
+++ lib/workspace.rb
@@ -1,25 +1,24 @@
 require 'httparty'
+require_relative 'user'
+require_relative 'channel'
 
 module SlackCLI
   class Workspace
     attr_reader :users, :channels
 
-    CHANNEL_PATH = "https://slack.com/api/conversations.list"
-    USER_PATH = "https://slack.com/api/users.list"
     def initialize
-      env = ENV["SLACK_API_TOKEN"]
-      users_response = HTTParty.get(USER_PATH, query: {token: ENV["SLACK_API_TOKEN"]})
-      channels_response = HTTParty.get(CHANNEL_PATH, query: {token: ENV["SLACK_API_TOKEN"]})
-      @users = users_response["members"]
-      @channels = channels_response["channels"]
+      @users = User.list_all
+      @channels = Channel.list_all
     end
+
     #user name, real name, slack Id
     def list_users
-      return @users.map{|user| "Slack ID: #{user["id"]}, Username: #{user["name"]}, Real name: #{user["real_name"]}"}
+      return @users.map{|user| "Slack ID: #{user.slack_id}, Username: #{user.name}, Real name: #{user.real_name}"}
     end
-    #topic, member count, slack ID topic["value"]
+
+    #topic, member count, slack ID, topic["value"]
     def list_channels
-      return @channels.map{|channel| "Slack ID: #{channel["id"]}, name: #{channel["name"]}, topic: #{channel["topic"]["value"]}, member count: #{channel["num_members"]}"}
+      return @channels.map{|channel| "Slack ID: #{channel.slack_id}, name: #{channel.name}, topic: #{channel.topic}, member count: #{channel.member_count}"}
     end
   end
 end
