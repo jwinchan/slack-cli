8674ed70cecd1363cd6abc9c0607e951dd37da0a
Iris Lux <iris.lux0@gmail.com>
2020-10-07 21:24:09 -0700

    added menu options for select

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 require 'dotenv'
 require 'httparty'
-require 'table_print'
+#require 'table_print'
 require_relative 'workspace'
 
 def main
@@ -11,15 +11,37 @@ def main
   workspace = SlackCLI::Workspace.new
 
   option = nil
-
-  until option == "quit" || option == "3"
+  current_selection = nil
+  until option == "quit" || option == "6"
     if option == "list users" || option == "1"
       puts workspace.list_users
     elsif option == "list channels" || option == "2"
       puts workspace.list_channels
+    elsif option == "select user" || option == "3"
+      puts "Are you entering the name or id?"
+      name_or_id = gets.chomp
+      if(name_or_id == "id")
+        puts "Please enter id"
+        current_selection = workspace.select(recipient_class: "user", id: gets.chomp)
+      else
+        puts "Please enter name"
+        current_selection = workspace.select(recipient_class: "user", name: gets.chomp)
+      end
+    elsif option == "select channel" || option == "4"
+      puts "Are you entering the name or id?"
+      name_or_id = gets.chomp
+      if(name_or_id == "id")
+        puts "Please enter id"
+        current_selection = workspace.select(recipient_class: "channel", id: gets.chomp)
+      else
+        puts "Please enter name"
+        current_selection = workspace.select(recipient_class: "channel", name: gets.chomp)
+      end
+    elsif option == "details" || option == "5"
+      puts current_selection.details
     end
     menu
-    option = gets.chomp
+    option = gets.chomp.downcase
   end
 
   puts "Thank you for using the Ada Slack CLI"
@@ -29,7 +51,10 @@ def menu
   puts "Please select one of the three options: "
   puts "1. list users"
   puts "2. list channels"
-  puts "3. quit"
+  puts "3. select user"
+  puts "4. select channel"
+  puts "5. details"
+  puts "6. quit"
 end
 
 main if __FILE__ == $PROGRAM_NAME
