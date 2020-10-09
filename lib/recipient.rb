c590b61190ea5d78d5e3de1119919519d0d2443c
Jessica Chan <jwinchan@gmail.com>
2020-10-08 21:32:07 -0700

    added send message method

--- lib/recipient.rb
+++ lib/recipient.rb
@@ -1,5 +1,6 @@
 require 'dotenv'
 require 'httparty'
+require_relative 'slackapierror'
 
 Dotenv.load
 
@@ -19,13 +20,12 @@ module SlackCLI
       end
     end
 
-    def self.get(url, params)
-      raise ArgumentError.new("invalid arguments") unless (url.is_a?(String) && params.is_a?(Hash))
-      response = HTTParty.get(url, query: params)
+    def self.get(end_point, params)
+      raise ArgumentError.new("invalid arguments") unless (end_point.is_a?(String) && params.is_a?(Hash))
+      response = HTTParty.get("https://slack.com/api/#{end_point}", query: params)
       unless response.code == 200 && response.parsed_response["ok"]
         raise SlackAPIError.new("Error: #{response.parsed_response["error"]}")
       end
-
       return response
     end
 
@@ -39,6 +39,26 @@ module SlackCLI
 
     def self.select(identifier)
       return self.list_all.find{|recipient| recipient.slack_id == identifier || recipient.name == identifier}
+    end
+
+    def send_message(message)
+
+      response = HTTParty.post(
+          "https://slack.com/api/chat.postMessage",
+          body:  {
+              token: ENV["SLACK_API_TOKEN"],
+              text: message,
+              channel: @slack_id,
+              as_user: "true"
+          },
+          headers: { 'Content-Type' => 'application/x-www-form-urlencoded' }
+      )
+
+      unless response.code == 200 && response.parsed_response["ok"]
+        raise SlackAPIError.new("Error: #{response.parsed_response["error"]}")
+      end
+
+      return true
     end
   end
 end
