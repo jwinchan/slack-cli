13723656005c9436a58887bdd2345899d40022ea
Jessica Chan <jwinchan@gmail.com>
2020-10-08 17:22:03 -0700

    required relative to new error file

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,13 +13,14 @@ require_relative '../lib/recipient'
 require_relative '../lib/user'
 require_relative '../lib/channel'
 require_relative '../lib/workspace'
+require_relative '../lib/slackapierror'
 
 Dotenv.load
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 VCR.configure do |config|
-  config.cassette_library_dir = "test/cassettes" # folder where casettes will be located
+  config.cassette_library_dir = "test/cassettes" # folder where cassettes will be located
   config.hook_into :webmock # tie into this other tool called webmock
   config.default_cassette_options = {
     :record => :new_episodes,    # record new data when we don't have it yet
