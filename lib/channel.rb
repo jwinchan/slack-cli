e334b4a8d8757904b1af3216e9e53454205cee91
Jessica Chan <jwinchan@gmail.com>
2020-10-08 21:30:42 -0700

    modified send message and channel history method

--- lib/channel.rb
+++ lib/channel.rb
@@ -3,8 +3,6 @@ require_relative 'recipient'
 module SlackCLI
   class Channel < Recipient
 
-    CHANNEL_PATH = "https://slack.com/api/conversations.list"
-
     attr_reader :slack_id, :name, :topic, :member_count
 
     def initialize(slack_id:, name:, topic:, member_count:)
@@ -14,13 +12,18 @@ module SlackCLI
     end
 
     def self.list_all
-      return self.get(CHANNEL_PATH, {token: ENV["SLACK_API_TOKEN"]})["channels"].map do |channel|
+      return self.get("conversations.list", {token: ENV["SLACK_API_TOKEN"]})["channels"].map do |channel|
         self.new(slack_id: channel["id"], name: channel["name"], topic: channel["topic"]["value"], member_count: channel["num_members"])
       end
     end
 
     def details
       return "ID: #{@slack_id} \nName: #{@name} \nTopic: #{@topic} \nMember Count: #{@member_count}"
+    end
+
+    def channel_history
+      response = Channel.get("conversations.history", {token: ENV["SLACK_API_TOKEN"], channel: @slack_id})
+      return response["messages"].map{ |message| message["text"] }
     end
   end
 end
